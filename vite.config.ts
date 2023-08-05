9c86865f244ed708ea66df6c3a294fc3a59dc344
Patrick Daguio <patrickdaguio@outlook.com>
2023-08-05 23:14:30 +0100

    BUILD: Folder structure, clean up, installed more dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/js/setupTests.ts",
+  },
+  resolve: {
+    alias: {
+      "@js": path.resolve(__dirname, "src/js"),
+      "@css": path.resolve(__dirname, "src/css"),
+    },
+  },
+});
